a166510d10642a0a053e712d4d33c2f7a87660ba
Gnezd <ycdzeng@gmail.com>
2024-11-12 15:48:39 +0800

    Parsing C header 土炮近完工

---
+++ test/dynamic_module_formation.rb
@@ -0,0 +1,75 @@
+require 'ffi'
+require 'pry'
+
+def parse_header(path)
+  source = File.open(path, 'r') {|f| f.readlines}
+  preproc_lines = []
+  structs = []
+  enums = []
+  constants = []
+  functions = []
+  other_declared = []
+  others = []
+
+  # To start line number at 1
+  source.unshift ''
+
+  # Depth recording and member buffering to deal with typedefs
+  in_struct = 0
+  in_enum = 0
+  member_buffer = []
+
+  source.each_index do |ln|
+    case source[ln]
+    when /^#/
+      preproc_lines.push ln
+      # Match for #define symbol val
+      match = source[ln].chomp.match(/^#define (\S+) (\S[\s\S]+)$/)
+      if match
+        constants.push match[1..2]
+      end
+
+    # Defining anonymous datatypes while aliasing?
+    when /^(?:\s*)typedef (struct|enum)(?:\s){?/ # Riskily taking optional { left bracket
+      type = source[ln].match(/^(?:\s*)typedef (struct|enum)(?:\s){?/)[1]
+      case type
+      when 'struct'
+        # Empty struct member lines buffer
+        member_buffer = []
+        in_struct += 1
+      when 'enum'
+        member_buffer = []
+        in_enum += 1
+      end
+    when /}(?:\s)(\S+);/
+      if in_struct > 0
+        in_struct -= 1
+        struct_name = source[ln].match(/}(?:\s)(\S+);/)[1]
+        structs.push({name: struct_name, members: member_buffer})
+      elsif in_enum > 0
+        in_enum -= 1
+        enum_name = source[ln].match(/}(?:\s)(\S+);/)[1]
+        enums.push({name: enum_name, members: member_buffer})
+      end
+
+    when /(?:unsigned)? int (\S+)\(([^\)]*)\);/
+      match = source[ln].match /(?:unsigned)? int (\S+)\(([^\)]*)\);/
+      functions.push({line: ln, name: match[1], args: match[2]})
+    when /^\/\//
+      # Comments
+    when /^\s+$/
+      # Blanks
+    else
+      # If still in a struct declaration scope?
+      if in_struct > 0 || in_enum > 0
+        member_buffer.push ln
+      else
+        others.push ln
+      end
+    end
+  end
+  binding.pry
+end
+
+parse_header("../include/atmcdLXd.h")
+binding.pry
