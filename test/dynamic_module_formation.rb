0376947634bb2dc37e757197e05d2e66b96690e6
Gnezd <ycdzeng@gmail.com>
2024-11-12 18:53:54 +0800

    Enum and struct now both parsed. Hex values stored as literal strings for now.

--- test/dynamic_module_formation.rb
+++ test/dynamic_module_formation.rb
@@ -1,12 +1,23 @@
 require 'ffi'
 require 'pry'
 
+# Parse symbol declaration
+def parse_symbol_dec(dec)
+  match = dec.match /([\s\S]+) (\S+)$/
+  raise " \"#{dec}\"<-- Doesn't look like a declaration" unless match
+  symbol = match[2]
+  pointer = match[1].include? "*"
+  type = match[1].gsub(/\s?\*\s?$/, '') # 去尾
+  type.gsub!(/^\s+/, '') # 去頭
+  [symbol, type, pointer]
+end
+
 def parse_header(path)
   source = File.open(path, 'r') {|f| f.readlines}
   preproc_lines = []
   structs = []
   enums = []
-  constants = []
+  constants = {}
   functions = []
   other_declared = []
   others = []
@@ -26,10 +37,11 @@ def parse_header(path)
       # Match for #define symbol val
       match = source[ln].chomp.match(/^#define (\S+) (\S[\s\S]+)$/)
       if match
-        constants.push match[1..2]
+        constants[match[1]] = match[2]
       end
 
     # Defining anonymous datatypes while aliasing?
+    # OK I know this is very wrong but works for this particular
     when /^(?:\s*)typedef (struct|enum)(?:\s){?/ # Riskily taking optional { left bracket
       type = source[ln].match(/^(?:\s*)typedef (struct|enum)(?:\s){?/)[1]
       case type
@@ -42,14 +54,26 @@ def parse_header(path)
         in_enum += 1
       end
     when /}(?:\s)(\S+);/
+      # Parse buffer lines
       if in_struct > 0
         in_struct -= 1
+        members = member_buffer.map do |ln|
+          chompped = source[ln].gsub(/[;\r\n]+/, '')
+          symbol, type, pointer = parse_symbol_dec(chompped)
+          {line: ln, symbol: symbol, type: type, pointer: pointer}
+        end
         struct_name = source[ln].match(/}(?:\s)(\S+);/)[1]
-        structs.push({name: struct_name, members: member_buffer})
+        structs.push({name: struct_name, members: members})
       elsif in_enum > 0
         in_enum -= 1
+        members = {}
+        member_buffer.each do |ln|
+          chomped = source[ln].gsub(/(^\s+)|[,\r\n]+$/, '')
+          key, value = chomped.split(/\s?=\s?/)
+          members[key] = value
+        end
         enum_name = source[ln].match(/}(?:\s)(\S+);/)[1]
-        enums.push({name: enum_name, members: member_buffer})
+        enums.push({name: enum_name, members: members})
       end
 
     # Looks like a function declaration
@@ -57,10 +81,8 @@ def parse_header(path)
       match = source[ln].match /(?:unsigned)? int (\S+)\(([^\)]*)\);/
       func_name = match[1]
       args = match[2].split(/\s?,\s?/).map do |arg|
-        type, symbol = arg.match(/^(\S[\S\s]*) (\S+)$/)[1..2]
-        pointer = type.include? "*"
-        type.gsub!(/\s*\*\s*$/, '')
-        [symbol, type, pointer]
+        symbol, type, pointer = parse_symbol_dec(arg)
+        {symbol: symbol, type: type, pointer: pointer}
       end
       functions.push({line: ln, name: func_name, args: args})
     when /^\/\//
@@ -69,15 +91,23 @@ def parse_header(path)
       # Blanks
     else
       # If still in a struct declaration scope?
-      if in_struct > 0 || in_enum > 0
-        member_buffer.push ln
+      if in_struct > 0
+        member_buffer.push ln if source[ln].include?(';') # Ugly catch of declaration line? Maybe not that ugly;
+      elsif in_enum > 0
+        member_buffer.push ln if source[ln].include?('=') # Enum assignment lines 
       else
         others.push ln
       end
     end
   end
-  binding.pry
+  {
+    structs: structs,
+    enums: enums, 
+    constants: constants,
+    functions: functions
+  }
 end
 
-parse_header("../include/atmcdLXd.h")
+atmcdLXd = parse_header("../include/atmcdLXd.h")
+
 binding.pry
